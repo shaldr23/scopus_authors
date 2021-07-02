94afe95855f6561e1918a45782215d71f5ac36b9
Alexander Shaposhnikov <shaldr23@gmail.com>
2021-07-02 11:54:50 +0300

    correcttion of authors initials

--- main.py
+++ main.py
@@ -7,24 +7,33 @@ import pandas as pd
 from collections import Counter
 import re
 
-FILE = 'scopus (4).csv'
-ORGANIZATION = 'Physical-Chemical Medicine' 
-RESULT_FILE = 'resultFCM_x.txt'
+FILE = 'scopus (18).csv'
+ORGANIZATION = 'Strategic Planning' 
+RESULT_FILE = 'result_x.txt'
 AUTHORS_NUMBERS = True
+CORRECT_INITS = True  # корректировать инициалы (Makarov V. -> Makarov V.V., если в списке есть оба варианта)
+
 frame = pd.read_csv(FILE, encoding='utf8')
 authors = []
 for rec in frame['Авторы организаций']:
     for auth_rec in rec.split(';'):
         if ORGANIZATION in auth_rec:
             lastn, inits, *trash = auth_rec.split(',')
-            authors.append(f'{lastn} {inits}')
+            author = f'{lastn} {inits}'
+            author = re.sub(r'\s+', ' ', author).strip()
+            authors.append(author)
+if CORRECT_INITS:
+    auth_set = set(authors)
+    for num, auth in enumerate(authors):
+        for auth_s in auth_set:
+            if auth_s.startswith(auth) and len(auth_s) > len(auth):
+                authors[num] = auth_s
 if not AUTHORS_NUMBERS:
     authors = ', '.join(set(authors))
 else:
     auth_nums = Counter(authors).most_common()
     authors = [f'{auth}({num})' for auth, num in auth_nums]
     authors = ', '.join(authors)
-authors = re.sub(r'\s+', ' ', authors).strip()
 with open(RESULT_FILE, 'w', encoding='utf') as f:
     f.write(authors)
 
